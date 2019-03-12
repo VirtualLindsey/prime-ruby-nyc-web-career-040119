d8391083451ff63c84261ac5a5523f6180316717
L D <tapircode@gmail.com>
2019-03-12 02:37:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(number)
+  if number < 0
+    number *= -1
+  end
+
+  if number == 0 || number == 1 || number == 2 || number == 3
+    return true
+  else
+    Prime.each(number) do |i|
+      if i < number
+        if number % i == 0
+          return false
+        end
+      end
+    end
+    return true
+  end
+end 
+
