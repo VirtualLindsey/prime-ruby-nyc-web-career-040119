ad2821e9748946b67cd7094d5715265e4a52f3df
L D <tapircode@gmail.com>
2019-03-12 02:41:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,13 @@ def prime?(number)
   else
     if number == 0 || number == 1
       return false
+    else if number == 2
+      return true
     else
+      primes = [2]
+      for i in 3..number
+        
+      end
       
     end
   end
