fa7f53e22fbef9799938906020bef44e492066e5
L D <tapircode@gmail.com>
2019-03-12 02:28:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,4 @@
 def prime?(number)
-  if number == 0 || number == 1 || number == 2 || number == 3 
-    return true 
-  end
-    
-  if number % 2 == 0 || number % 3 == 0
-    return false
-  else 
-    return true 
-  end 
+  Prime.
 end 
 
